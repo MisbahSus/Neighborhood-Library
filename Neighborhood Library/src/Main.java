9d15d23fb8a6f851e86ff1ff280789c49091e3fc
MisbahSus <misbah859@gmail.com>
2025-04-18 16:51:31 -0700

    Add files via upload

---
+++ Neighborhood Library/src/Main.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class Main {
+    static Book[] books = new Book[20];
+
+    static void available(){
+        System.out.println("\nAVAILABLE:");
+        for(Book b : books){
+            if(b == null || b.isCheckedOut){
+                continue;
+            }
+            System.out.printf("ID: %d - %s, %s\n", b.id, b.title, b.isbn);
+        }
+        String command = "start";
+        Scanner in = new Scanner(System.in);
+        while(!command.equalsIgnoreCase("X")){
+            System.out.println("""
+C - to check out a book.
+X - to exit this screen.
+                    """);
+            command = in.nextLine().trim();
+            if(command.equalsIgnoreCase("X")){
+                return; // exit function immediately
+            }else if(command.equalsIgnoreCase("C")){
+                System.out.println("Enter book id: ");
+                int id = in.nextInt();
+                in.nextInt(); // throw away trailing new line
+                for(Book b : books){
+                    if(b.id == id){
+                        System.out.println("Enter your name: ");
+                        String name = in.nextLine().trim();
+                        b.checkOut("USER");
+                        break;
+                    }
+                }
+            } // end else if
+        } // end while
+    }
+    static void checkedOut(){
+        System.out.println("\nCHECKED OUT:");
+        for(Book b : books){
+            if(b == null || !b.isCheckedOut){
+                continue;
+            }
+            System.out.printf("%d - %s, %s\n",b.id, b.title, b.checkedOutTo);
+        }
+        String command = "start";
+        Scanner in = new Scanner(System.in);
+        while(!command.equalsIgnoreCase("X")){
+            System.out.println("""
+C - to check in a book.
+X - to exit this screen
+                    """);
+            command = in.nextLine().trim();
+            if(command.equalsIgnoreCase("X")){
+                return; // exit function/method immediately
+            }else if(command.equalsIgnoreCase("C")){
+                System.out.println("Enter id of book to check in: ");
+                int id = in.nextInt();
+                if (!command.isEmpty()) {
+                    for (Book b : books) {
+                        if(b.id == id) {
+                            b.checkIn();
+                            break; // exit loop
+                        }
+                    }
+                }
+            }else{
+                System.out.println("No such command at this level: " + command);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        books[0] = new Book(10,"978-030729063", "Foundation");
+        books[1] = new Book(23,"ASIN-B07D23CFGR", "Atomic Habits");
+        books[2] = new Book(2006,"978-0679728757", "Blood Meridian");
+//        books[1].checkOut("Misbah");
+//        available();
+//        checkedOut();
+        Scanner in = new Scanner(System.in);
+        int command = -1;
+        while(command != 0) {
+            System.out.println("\nMAIN MENU:");
+            System.out.print("""
+                    1 - Show Available Books
+                    2 - Show Checked Out Books
+                    0 - Exit - closes out of the application
+                    """);
+            command = in.nextInt();
+            if (command == 0) {
+                break; // exit loop right now!!!
+            } else if (command == 1) {
+                // show available plus menu
+                available();
+            } else if (command == 2) {
+                // show checked out plus menu
+                checkedOut();
+            } else {
+                // no such command
+                System.out.println("no such command: " + command);
+            }
+        } // end outer while loop
+        System.out.println("Thanks for using the Library!!!");
+    } // end main program
+}
